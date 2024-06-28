51c4981bc9861a8e0c4f73901f529dfa4ee69658
Yancy Alfaro <alfaromargarita556@gmail.com>
2024-06-27 18:02:47 -0600

    Update  para categorias y subCategorias

--- backend/routes/categoryRoutes/categoryRoutes.js
+++ backend/routes/categoryRoutes/categoryRoutes.js
@@ -4,6 +4,7 @@ import verifyToken from "../../middlewares/tokenUserAccess.js";
 import { CreateCategory } from "../../controllers/category/CreateCategoryController.js";
 import { GetCategories, GetCategory, DeleteCategory } from '../../controllers/category/getAndDeleteCategoryController.js';
 import { validationMiddleware } from "../../middlewares/validationMiddleware.js";
+import { UpdateCategory } from '../../controllers/category/UpdateCategoryController.js';
 
 const categoryRouter = Router();
 
@@ -15,7 +16,9 @@ categoryRouter.post(
     validationMiddleware,
     CreateCategory
 );
+categoryRouter.put('/categories/:id', verifyToken, UpdateCategory);
 categoryRouter.get('/categories', verifyToken, GetCategories);
 categoryRouter.get('/categories/:id', verifyToken, GetCategory);
 categoryRouter.delete('/categories/:id', verifyToken, DeleteCategory);
+
 export default categoryRouter;

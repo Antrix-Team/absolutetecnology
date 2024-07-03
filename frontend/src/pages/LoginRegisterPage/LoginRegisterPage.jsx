b343ce9fa8ab80a8058c5cb7e188c8b285b26fd3
Yancy Alfaro <alfaromargarita556@gmail.com>
2024-07-02 21:02:09 -0600

    Modal registro usuario

--- frontend/src/pages/LoginRegisterPage/LoginRegisterPage.jsx
+++ frontend/src/pages/LoginRegisterPage/LoginRegisterPage.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import LoginRegister from "../../components/LoginRegister/LoginRegister";
+import CreateUserModal from "../../components/ModalUser/CreateUserModal";
 
 const LoginRegisterPage = () => {
     return (
         <div>
-            <LoginRegister />
+            <CreateUserModal />
         </div>
     );
 };
